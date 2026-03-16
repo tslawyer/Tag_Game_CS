770c79ade83b60fea0c928f7f4dde5f99a6a2840
Nick <melancholic8078@gmail.com>
2026-03-16 02:26:58 +0200

    added user enter field size

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -3,9 +3,32 @@
 
 
 int main() {
-    GameField game;
 
-    game.init(4);
+    GameField game;
+    int size;
+
+    while (true) {
+
+        std::cout << "Enter field size: ";
+        std::cin >> size;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "Invalid input. Enter a number.\n";
+            continue;
+        }
+
+        if (size <= 1) {
+            std::cout << "Size must be greater than 1\n";
+            continue;
+        }
+
+        break;
+    }
+
+    game.init(size);
     game.print();
+
     return 0;
 }
